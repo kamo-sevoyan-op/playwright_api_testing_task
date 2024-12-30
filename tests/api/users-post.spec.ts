81d9bac7e8c97ae5b8d48e788d1f880877793ecb
kamo-sevoyan-op <kamo.sevoyan@optimumpartners.co>
2024-12-30 18:26:47 +0400

    Add users post endpoint tests.

---
+++ tests/api/users-post.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+import { validateRequest } from './utils';
+
+const USERS_ENDPOINT = 'api/users';
+
+test.describe(`Tests for ${USERS_ENDPOINT}|POST endpoint`, () => {
+  test('Should add new user data', async ({ request }) => {
+    const userData = {
+      name: 'John',
+      job: 'Engineer',
+    };
+
+    const beforePost = new Date().getTime();
+    const response = await request.post(USERS_ENDPOINT, { data: userData });
+
+    const data = await validateRequest(response, 201);
+
+    expect(data).toBeTruthy();
+    expect(data).toHaveProperty('name', userData.name);
+    expect(data).toHaveProperty('job', userData.job);
+    expect(data).toHaveProperty('id');
+    expect(data).toHaveProperty('createdAt');
+
+    const createdAt = new Date(data.createdAt).getTime();
+    expect(beforePost).toBeLessThanOrEqual(createdAt);
+  });
+});
