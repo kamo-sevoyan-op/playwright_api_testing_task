27fb09f410f44652ed06a8795017b4948da38c85
kamo-sevoyan-op <kamo.sevoyan@optimumpartners.co>
2024-12-30 19:00:18 +0400

    Add login endpoint tests.

---
+++ tests/api/login-post.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+import { validateRequest } from './utils';
+
+const LOGIN_ENDPOINT = 'api/login';
+const { LOGIN_EMAIL, LOGIN_PASSWORD } = process.env;
+
+test.describe(`Tests for ${LOGIN_ENDPOINT}|POST endpoint`, () => {
+  test('Should login the user', async ({ request }) => {
+    const credentials = {
+      email: LOGIN_EMAIL ?? '',
+      password: LOGIN_PASSWORD ?? '',
+    };
+
+    const response = await request.post(`${LOGIN_ENDPOINT}`, {
+      data: credentials,
+    });
+
+    const data = await validateRequest(response);
+
+    expect(data).toHaveProperty('token');
+  });
+
+  test('Should fail to login with invalid credentials', async ({ request }) => {
+    const credentials = {
+      email: LOGIN_EMAIL ?? '',
+    };
+
+    const response = await request.post(`${LOGIN_ENDPOINT}`, {
+      data: credentials,
+    });
+
+    expect(response.ok()).toBeFalsy();
+    expect(response.status()).toBe(400);
+  });
+});
